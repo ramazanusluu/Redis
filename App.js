4e9bf7d0ea9b0e2bd021f87a596c22ea3b75a15a
Ramazan <ruslu1720@gmail.com>
2022-05-13 09:00:15 +0300

    Nodemon paket yonetimi eklendi

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import { createClient } from "redis";
 
+//get set gibi işelmleri yapabilmek için client üretmemiz gerekiyor
 const client = createClient({
   url: "redis://localhost:6391",
+  //defalult olarak 6379 portuna bağlanıyor. bunun için redis://localhost:6391 eklendi.
 });
-client.on("error", (err) => console.log("Redis Client Error", err));
 
+//client'ın bağlanmasında herhangi bir problem varsa on ile bu eventi alabiliyoruz.
+client.on("error", (err) => console.log("Redis Client Error", err));
 await client.connect();
-//Set işlemi
-const setislem = await client.set("test", "test");
-console.log("Set: ", setislem);
-//Get işlemi
+
+//set işlemi
+const setislemi = await client.set("test", "test");
+console.log("Set: ", setislemi);
+
+//get işlemi
 const value = await client.get("key");
 console.log("Get: ", value);
 
+//del işlemi
+const del = await client.del("test");
+console.log("Del: ", del);
+
+//exists işlemi
+const exists = await client.exists("key");
+console.log("Exists: ", exists);
+
+//hset işelmi
+const hset = await client.hSet("user", "name", "Asya");
+console.log("hset : ", hset);
